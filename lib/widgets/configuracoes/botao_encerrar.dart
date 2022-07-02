de94ef7cea0978088c13f622a5923285d0be2add
Rafael dos Santos <rafaels1997@live.com>
2022-07-02 20:30:33 -0300

    Correções do layout

--- lib/widgets/configuracoes/botao_encerrar.dart
+++ lib/widgets/configuracoes/botao_encerrar.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 
 class BotaoEncerrarSessao extends StatelessWidget {
+  const BotaoEncerrarSessao({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 20),
       child: ElevatedButton(
         onPressed: () {},
-        child: Text(
-          "Encerrar Sessão",
-          style: TextStyle(fontSize: 18),
-        ),
         style: ButtonStyle(
             shape: MaterialStateProperty.all<RoundedRectangleBorder>(
           RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(18.0),
               side: const BorderSide(color: Colors.blue)),
         )),
+        child: const Text(
+          "Encerrar Sessão",
+          style: TextStyle(fontSize: 18),
+        ),
       ),
     );
   }
